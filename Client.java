4bc005bc869b174acb407538cee08f678414088b
sowmyathota <sowmya.thota50@gmail.com>
2017-06-07 17:31:56 +0530

    added comments

--- Client.java
+++ Client.java
@@ -6,8 +6,8 @@ public static void main(String args[])
 try{
 Socket s=new Socket("localhost",6555);
 DataOutputStream dout=new DataOutputStream(s.getOutputStream());
-dout.writeUTF("HELLO SERVER");
-dout.flush();
+dout.writeUTF("HELLO SERVER");//wriring into the server
+dout.flush();//flushing 
 dout.close() ;
 s.close();//closing socket
 }
