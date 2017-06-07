be95198fc49b7d0691f43c998a7d7f1a2a35b69c
sowmyathota <sowmya.thota50@gmail.com>
2017-06-07 16:44:55 +0530

    hello server

---
+++ Server.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.net.*;
+public class Server{
+public static void main(String args[])
+{
+try{
+ServerSocket ss=new ServerSocket(6555);
+Socket s=ss.accept();
+DataInputStream dis=new DataInputStream(s.getInputStream());
+String str=(String)dis.readUTF();
+System.out.println("message = "+str);
+ss.close();
+}
+catch(Exception e)
+{
+System.out.println(e);
+}
+
+}
+
+}
