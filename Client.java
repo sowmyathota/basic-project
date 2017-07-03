da878051824e8fedf361b92bfb69012a0520f319
sowmyathota <sowmya.thota50@gmail.com>
2017-07-03 12:34:24 +0530

    A

--- Client.java
+++ Client.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.net.*;
 public class Client{
-public static void main(String args[])
+public static void main(String args[])  //main method
 {
 try{
 Socket s=new Socket("localhost",6555);
